c251113a4b3c5a85d8ccd78e42228e8cbb70abf9
Daniel Smółka <smowling@gmail.com>
2024-04-15 19:42:52 +0200

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,26 @@
 <template>
     <div>
         <div class="container">
-            <div id="boxA" class="box" @dragover="dragOver" @drop="drop">{{ zone1[zone1.length - 1] }}
+            <div id="asc1" class="box" @dragover="dragOver" @drop="drop">{{ asc1[asc1.length - 1] }}
             </div>
-            <div id="boxB" class="box" @dragover="dragOver" @drop="drop">{{ zone2[zone2.length - 1] }}
-
+            <div id="asc2" class="box" @dragover="dragOver" @drop="drop">{{ asc2[asc2.length - 1] }}
             </div>
-
-            <div class="container">
-                <div id="1" draggable="true" class="box" @dragstart="dragStart">A</div>
-                <div id="2" draggable="true" class="box" @dragstart="dragStart">B</div>
-                <div id="3" draggable="true" class="box" @dragstart="dragStart">C</div>
-            </div>
+        </div>
+        <div class="container">
+            <div id="1" draggable="true" class="card" @dragstart="dragStart">2</div>
+            <div id="2" draggable="true" class="card" @dragstart="dragStart">3</div>
+            <div id="3" draggable="true" class="card" @dragstart="dragStart">4</div>
         </div>
     </div>
 </template>
 
 <script setup>
 
-const zone1 = ref(['zone1'])
-const zone2 = ref(['zone2'])
+const asc1 = ref(['1'])
+const asc2 = ref(['1'])
+
+
+
 
 const dragOver = (event) => {
     event.preventDefault(); // Necessary to allow dropping
@@ -34,10 +35,10 @@ const drop = (event) => {
     // Prevent dropping if the target is also draggable
     if (!targetElement.getAttribute('draggable')) {
         targetElement.appendChild(draggedElement);
-        if (targetElement.id === 'boxA') {
-            zone1.value.push(draggedElement.textContent);
-        } else if (targetElement.id === 'boxB') {
-            zone2.value.push(draggedElement.textContent);
+        if (targetElement.id === 'asc1') {
+            asc1.value.push(draggedElement.textContent);
+        } else if (targetElement.id === 'asc2') {
+            asc2.value.push(draggedElement.textContent);
         }
     }
 };
@@ -63,5 +64,14 @@ const dragStart = (event) => {
     border-radius: .5em;
     padding: 10px;
     cursor: move;
+}
+
+.card {
+    border: 4px solid #000;
+    background-color: #ddd;
+    border-radius: .5em;
+    padding: 10px;
+    height: 80px;
+    width: 54px;
 }
 </style>
