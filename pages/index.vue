09e575dd179b77471665a61eaa1afa9f3ec6b837
Daniel Smółka <smowling@gmail.com>
2024-04-15 15:26:34 +0200

    drag testing

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<<template>
+    <div>
+        <div class="container">
+            <div draggable="true" class="box">A</div>
+            <div draggable="true" class="box">B</div>
+            <div draggable="true" class="box">C</div>
+        </div>
+    </div>
+</template>
+
+    <script setup>
+
+</script>
+
+    <style scoped>
+
+    .container {
+        display: grid;
+        grid-template-columns: repeat(5, 1fr);
+        gap: 10px;
+    }
+
+    .box {
+        border: 3px solid #666;
+        background-color: #ddd;
+        border-radius: .5em;
+        padding: 10px;
+        cursor: move;
+    }
+</style>
