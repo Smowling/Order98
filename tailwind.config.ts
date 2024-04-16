20122ae4978bdcac738c232390cd7d70f0529ff1
Daniel Smółka <smowling@gmail.com>
2024-04-16 12:04:26 +0200

    tw conf

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'tailwindcss'
+import colors from 'tailwindcss/colors'
+
+export default <Partial<Config>>{
+    theme: {
+        container: {
+            center: true,
+        },
+        extend: {
+            colors: {
+                primary: colors.gray
+            }
+        }
+    }
+}
