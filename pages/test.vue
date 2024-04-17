e6d8cca5f946abeb0c7c115d5d4a0743b7a34d25
Daniel Smółka <smowling@gmail.com>
2024-04-17 20:42:59 +0200

    learning css

--- pages/test.vue
+++ pages/test.vue
@@ -1,11 +1,20 @@
 <template>
-    <div class="container">
+    <!-- <div class="grid justify-items: center;"> -->
+    <div class="game justify-self: center">
 
     </div>
+    <!-- </div> -->
 </template>
 
 <script setup lang="ts">
 
 </script>
 
-<style scoped></style>
+<style scoped>
+.game {
+    width: 98%;
+    max-width: 800px;
+    background-color: blue;
+    height: 1000px;
+}
+</style>
