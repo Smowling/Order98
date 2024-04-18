b8c37f40cb80e06666902e879e3fecdb94e555ab
Daniel Smółka <smowling@gmail.com>
2024-04-18 12:31:59 +0200

    template

--- pages/test.vue
+++ pages/test.vue
@@ -1,6 +1,7 @@
 <template>
     <!-- <div class="grid justify-items: center;"> -->
     <div class="game">
+        <nav>test</nav>
         <div class="dropzone">
             <div class="zone"></div>
             <div class="zone"></div>
@@ -30,7 +31,10 @@
     width: 100%;
     height: 45%;
     background-color: bisque;
-
+    display: grid;
+    grid-template-rows: 50% 50%;
+    grid-template-columns: 50% 50%;
+    justify-content: center;
 }
 
 .hand {
@@ -41,10 +45,11 @@
 
 .zone {
     background-color: cadetblue;
-    width: 50%;
-    height: 50%;
-    border: 1px;
+    width: 95%;
+    height: 95%;
+    border: 5px;
     border-radius: 5px;
     border-color: black;
+    justify-content: center;
 }
 </style>
