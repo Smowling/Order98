ff4ec98151a9a0893399a7532d1c40bc27012543
Daniel Smółka <smowling@gmail.com>
2024-04-27 09:23:15 +0200

    hand comp inprog

---
+++ src/components/Card.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="card flex-wrap" v-for="card in game.deck">{{ card }}</div>
+    </div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style scoped>
+.card {
+    width: 48px;
+    height: 84px;
+    background-color: rgb(157, 75, 171);
+}
+</style>
