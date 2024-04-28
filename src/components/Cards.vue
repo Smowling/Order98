2d48d1fb5b11d2b73b865179d874417dd58fc096
Daniel Smółka <smowling@gmail.com>
2024-04-28 15:13:33 +0200

    cards comp

---
+++ src/components/Cards.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="grid grid-cols-4 gap-4">
+        <div class="card flex-wrap" v-for="card in game.hand" draggable="true" @dragstart="dragStart">{{ card }}</div>
+    </div>
+</template>
+
+<script lang="ts">
+const game = useGameStore();
+
+const dragStart = (event: any) => {
+    event.dataTransfer.setData("text", event.target.id);
+};
+</script>
+
+<style scoped>
+.card {
+    width: 48px;
+    height: 84px;
+    background-color: rgb(157, 75, 171);
+}
+</style>
