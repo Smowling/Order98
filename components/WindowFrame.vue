2b26b7a5e938af1ac0ae0c2db3f61c0806f38cd8
Daniel Smółka <smowling@gmail.com>
2024-04-29 14:01:13 +0200

    changes to window

--- components/WindowFrame.vue
+++ components/WindowFrame.vue
@@ -1,7 +1,5 @@
 <template>
-    <div class="mockup-window border bg-base-300 w-1/3 min-w-96 bg-cyan-600">
-        <div class="flex justify-center px-4 py-16 bg-cyan-200">
-            <slot></slot>
-        </div>
+    <div class="artboard phone-3 border bg-cyan-600 ">
+        <slot></slot>
     </div>
 </template>
