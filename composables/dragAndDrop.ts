722175507f1f57c0ba5f857af1b655fbebbe3e18
Daniel Smółka <smowling@gmail.com>
2024-05-04 15:03:25 +0200

    dnd validplay and drop mod

--- composables/dragAndDrop.ts
+++ composables/dragAndDrop.ts
@@ -14,33 +14,29 @@ export default function useDragAndDrop() {
 
         const cardIndex = parseInt(draggedElement.id, 10);
         const pileValue = parseInt(targetElement.textContent, 10);
+        const cardValue = parseInt(draggedElement.textContent, 10)
         // console.log(targetElement.textContent) target value
 
         // Prevent dropping if the target is also draggable
         if (!targetElement.getAttribute('draggable')) {
             // targetElement.appendChild(draggedElement);
             if (targetElement.id === 'asc1') {
-                console.log("dragged to asc1")
-                if (cardIndex > -1 && validPlay(true, pileValue, cardIndex)) {
-                    console.log(pileValue, cardIndex)
+                if (cardIndex > -1 && validPlay(true, pileValue, cardValue)) {
                     game.piles.asc1.push(...game.hand.splice(cardIndex, 1));
                 }
             }
             if (targetElement.id === 'asc2') {
-                console.log("dragged to asc2")
-                if (cardIndex > -1) {
+                if (cardIndex > -1 && validPlay(true, pileValue, cardValue)) {
                     game.piles.asc2.push(...game.hand.splice(cardIndex, 1));
                 }
             }
             if (targetElement.id === 'desc1') {
-                console.log("dragged to desc1")
-                if (cardIndex > -1) {
+                if (cardIndex > -1 && validPlay(false, pileValue, cardValue)) {
                     game.piles.desc1.push(...game.hand.splice(cardIndex, 1));
                 }
             }
             if (targetElement.id === 'desc2') {
-                console.log("dragged to desc2")
-                if (cardIndex > -1) {
+                if (cardIndex > -1 && validPlay(false, pileValue, cardValue)) {
                     game.piles.desc2.push(...game.hand.splice(cardIndex, 1));
                 }
             }
@@ -53,6 +49,7 @@ export default function useDragAndDrop() {
 
     const validPlay = (asc: boolean, pileValue: number, cardValue: number) => {
         if (asc) {
+            console.log('we have asc, ', pileValue, cardValue, cardValue > pileValue, pileValue - cardValue === 10)
             if (cardValue > pileValue || pileValue - cardValue === 10) {
                 return true;
             }
