6c6770ae0017d15ab61467fe4cf2703eba4c07bb
Daniel Smółka <smowling@gmail.com>
2024-05-04 14:51:47 +0200

    validplay function

--- composables/dragAndDrop.ts
+++ composables/dragAndDrop.ts
@@ -11,16 +11,18 @@ export default function useDragAndDrop() {
         const data = event.dataTransfer.getData("text");
         const targetElement = event.target;
         const draggedElement = document.getElementById(data);
-        const cardIndex = parseInt(draggedElement.id, 10);
 
+        const cardIndex = parseInt(draggedElement.id, 10);
+        const pileValue = parseInt(targetElement.textContent, 10);
         // console.log(targetElement.textContent) target value
 
         // Prevent dropping if the target is also draggable
         if (!targetElement.getAttribute('draggable')) {
-            targetElement.appendChild(draggedElement);
+            // targetElement.appendChild(draggedElement);
             if (targetElement.id === 'asc1') {
                 console.log("dragged to asc1")
-                if (cardIndex > -1) {
+                if (cardIndex > -1 && validPlay(true, pileValue, cardIndex)) {
+                    console.log(pileValue, cardIndex)
                     game.piles.asc1.push(...game.hand.splice(cardIndex, 1));
                 }
             }
@@ -48,5 +50,18 @@ export default function useDragAndDrop() {
     const dragStart = (event: any) => {
         event.dataTransfer.setData("text", event.target.id);
     };
+
+    const validPlay = (asc: boolean, pileValue: number, cardValue: number) => {
+        if (asc) {
+            if (cardValue > pileValue || pileValue - cardValue === 10) {
+                return true;
+            }
+        } else {
+            if (cardValue < pileValue || cardValue - pileValue === 10) {
+                return true;
+            }
+        }
+        return false;
+    }
     return { dragOver, drop, dragStart };
 }
