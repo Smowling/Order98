ff399d3019592c8098ee995d7b2924b06d3ebd78
Daniel Smółka <smowling@gmail.com>
2024-05-05 23:12:53 +0200

    dnd cleanup

--- composables/dragAndDrop.ts
+++ composables/dragAndDrop.ts
@@ -1,11 +1,22 @@
+/**
+ * Custom hook for implementing drag and drop functionality in a game.
+ * @returns {Object} - Object containing dragOver, drop, and dragStart functions.
+ */
 export default function useDragAndDrop() {
-
     const game = useGameStore();
 
+    /**
+     * Prevents the default behavior of the drag event.
+     * @param {DragEvent} event - The drag event.
+     */
     const dragOver = (event: DragEvent) => {
-        event.preventDefault(); // Necessary to allow dropping
+        event.preventDefault();
     };
 
+    /**
+     * Handles the drop event and performs actions based on the target element's ID and the validity of the play.
+     * @param {DragEvent} event - The drop event.
+     */
     const drop = (event: any) => {
         event.preventDefault();
         const data = event.dataTransfer.getData("text");
@@ -14,42 +25,43 @@ export default function useDragAndDrop() {
 
         const cardIndex = parseInt(draggedElement.id, 10);
         const pileValue = parseInt(targetElement.textContent, 10);
-        const cardValue = parseInt(draggedElement.textContent, 10)
-        // console.log(targetElement.textContent) target value
+        const cardValue = parseInt(draggedElement.textContent, 10);
 
         // Prevent dropping if the target is also draggable
-        if (!targetElement.getAttribute('draggable')) {
-            // targetElement.appendChild(draggedElement);
-            if (targetElement.id === 'asc1') {
-                if (cardIndex > -1 && validPlay(true, pileValue, cardValue)) {
-                    game.piles.asc1.push(...game.hand.splice(cardIndex, 1));
-                }
+        if (!targetElement.getAttribute("draggable")) {
+            if (targetElement.id === "asc1" && cardIndex > -1 && validPlay(true, pileValue, cardValue)) {
+                game.piles.asc1.push(...game.hand.splice(cardIndex, 1));
             }
-            if (targetElement.id === 'asc2') {
-                if (cardIndex > -1 && validPlay(true, pileValue, cardValue)) {
-                    game.piles.asc2.push(...game.hand.splice(cardIndex, 1));
-                }
+            if (targetElement.id === "asc2" && cardIndex > -1 && validPlay(true, pileValue, cardValue)) {
+                game.piles.asc2.push(...game.hand.splice(cardIndex, 1));
             }
-            if (targetElement.id === 'desc1') {
-                if (cardIndex > -1 && validPlay(false, pileValue, cardValue)) {
-                    game.piles.desc1.push(...game.hand.splice(cardIndex, 1));
-                }
+            if (targetElement.id === "desc1" && cardIndex > -1 && validPlay(false, pileValue, cardValue)) {
+                game.piles.desc1.push(...game.hand.splice(cardIndex, 1));
             }
-            if (targetElement.id === 'desc2') {
-                if (cardIndex > -1 && validPlay(false, pileValue, cardValue)) {
-                    game.piles.desc2.push(...game.hand.splice(cardIndex, 1));
-                }
+            if (targetElement.id === "desc2" && cardIndex > -1 && validPlay(false, pileValue, cardValue)) {
+                game.piles.desc2.push(...game.hand.splice(cardIndex, 1));
             }
         }
         game.playCard();
     };
+
+    /**
+     * Sets the data to be transferred during the drag operation.
+     * @param {DragEvent} event - The drag event.
+     */
     const dragStart = (event: any) => {
         event.dataTransfer.setData("text", event.target.id);
     };
 
+    /**
+     * Checks if a play is valid based on the ascending or descending order and the values of the pile and card.
+     * @param {boolean} asc - Indicates if the play is ascending or descending.
+     * @param {number} pileValue - The value of the pile.
+     * @param {number} cardValue - The value of the card.
+     * @returns {boolean} - True if the play is valid, false otherwise.
+     */
     const validPlay = (asc: boolean, pileValue: number, cardValue: number) => {
         if (asc) {
-            console.log('we have asc, ', pileValue, cardValue, cardValue > pileValue, pileValue - cardValue === 10)
             if (cardValue > pileValue || pileValue - cardValue === 10) {
                 return true;
             }
@@ -59,6 +71,7 @@ export default function useDragAndDrop() {
             }
         }
         return false;
-    }
+    };
+
     return { dragOver, drop, dragStart };
 }
