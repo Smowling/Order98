9eec877ebaaaf4e0e6d78d51713904c291d3e2a3
Daniel Smółka <smowling@gmail.com>
2024-05-06 14:18:01 +0200

    tweaks

--- pages/test.vue
+++ pages/test.vue
@@ -1,19 +1,18 @@
 <template>
-    <WindowFrame>
-        <div class="window-frame">
-            <nav>
-                <Drawer />
-                <button @click="game.restart">Restart</button>
-            </nav>
-            <DropZones />
-            <div class="content-expand"></div> <!-- This div will push the BottomNav to the bottom -->
-
-            <Cards />
-
-            <BottomNav />
-        </div>
-    </WindowFrame>
-
+    <container class="window-frame">
+        <WindowFrame>
+            <div class="window-frame">
+                <nav>
+                    <Drawer />
+                    <button @click="game.restart">Restart</button>
+                </nav>
+                <DropZones />
+                <div class="content-expand"></div> <!-- This div will push the BottomNav to the bottom -->
+                <Cards />
+                <BottomNav />
+            </div>
+        </WindowFrame>
+    </container>
 </template>
 
 <script setup lang="ts">
@@ -38,6 +37,10 @@ onMounted(() => {
 .window-frame {
     display: flex;
     flex-direction: column;
+    justify-content: center;
+    /* Centers content vertically */
+    align-items: center;
+    /* Centers content horizontally */
     height: 100%;
     /* Ensure WindowFrame takes full height */
 }
