341f4d97860b3e550e0867f5d74c52d3f3179abf
Daniel Smółka <smowling@gmail.com>
2024-05-09 14:03:39 +0200

    grid tests

---
+++ pages/gridtest.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <div>test1</div>
+        <div>test1</div>
+        <div>test1</div>
+        <div>test1</div>
+        <div>test1</div>
+        <div>test1</div>
+        <div>test1</div>
+        <div>test1</div>
+        <div>test1</div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.container {
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    grid-template-rows: repeat(10, 1fr);
+    height: 100vh;
+    /* 100% of the viewport height */
+    width: 100vw;
+    /* 100% of the viewport width */
+    gap: 10px;
+    padding: 50px;
+    /* Adjust the gap as needed */
+}
+</style>
