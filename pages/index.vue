2c6b35db19bd906d19026cfaee957ebc7e92696d
Daniel Smółka <smowling@gmail.com>
2024-05-23 17:18:02 +0200

    index

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,33 @@
 <template>
-    <div>
-        index
-    </div>
+    <WindowFrame>
+        <TopNav />
+        <DropZones />
+        <!-- T<div class="content-expand"></div> his div will push the BottomNav to the bottom -->
+        <Cards />
+        <BottomNav />
+    </WindowFrame>
 </template>
 
-<script setup>
+<script setup lang="ts">
 
+const game = useGameStore();
+
+onMounted(() => {
+    // start game or load from localstorage
+    if (!localStorage.getItem('gameState')) {
+        game.setup();
+    }
+    else {
+        const gameState = JSON.parse(localStorage.getItem('gameState')!);
+        game.load(gameState);
+    }
+})
 
 
 </script>
 
-
-<style scoped></style>
+<style scoped>
+.content-expand {
+    flex-grow: 1;
+}
+</style>
