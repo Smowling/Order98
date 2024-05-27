d07661ac1dd4e64381af146663798b46f41201ca
Daniel Smółka <smowling@gmail.com>
2024-05-27 15:31:50 +0200

    restart handler

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -13,7 +13,7 @@
             <a class="btn btn-ghost text-xl">Order98</a>
         </div>
         <div class="flex-none">
-            <button class="btn btn-square btn-ghost">
+            <button class="btn btn-square btn-ghost" @click="game.restart">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                     class="inline-block w-5 h-5 stroke-current">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -26,7 +26,7 @@
 </template>
 
 <script setup>
-
+const game = useGameStore();
 </script>
 
 <style lang="scss" scoped></style>
