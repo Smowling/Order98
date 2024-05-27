f422db937c37b8a46455ecd263d2c4632c03602d
Daniel Smółka <smowling@gmail.com>
2024-05-27 12:14:25 +0200

    settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Order98',
+      titleTemplate: '%s'
+    }
+  },
+  $development: {
+    app: {
+      head: {
+        title: 'DEV Order98'
+      }
+    }
+  },
   devtools: { enabled: true },
   modules: ['@pinia/nuxt', "@nuxt/ui", "@formkit/auto-animate"]
 })
