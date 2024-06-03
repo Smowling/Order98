2140a4da1e9c32eb8d84ac98ac1a7e0868648d0e
Daniel Smółka <smowling@gmail.com>
2024-06-03 20:22:19 +0200

    botnav

--- components/BottomNav.vue
+++ components/BottomNav.vue
@@ -1,26 +1,15 @@
 <template>
-    <ul class="menu bg-base-200 lg:menu-horizontal rounded-box">
-        <li>
-            <button @click="game.undo">
-                undo
-            </button>
-        </li>
-        <li>
-            cards {{ game.deck.length }}
-        </li>
-        <li v-if="game.hand.length <= game.handSize - 2">
-            <button @click="game.draw">
-                draw
-            </button>
-        </li>
-        <li v-else class="disabled">
-            <button>
-                draw
-            </button>
-        </li>
-    </ul>
+    <UHorizontalNavigation :links="links" class="border-b border-gray-200 dark:border-gray-800" />
 </template>
 <script setup lang="ts">
 const game = useGameStore();
+const links = [
+    {
+        label: 'Deck: '
+    }, {
+        label: 'Draw',
+        click: game.draw
+    }
+]
 
 </script>
