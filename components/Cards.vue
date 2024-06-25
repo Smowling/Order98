ea2dbd7f8ae040d51726753c69d2afb921eada4a
Daniel Smółka <smowling@gmail.com>
2024-06-25 15:55:05 +0200

    grid rows

--- components/Cards.vue
+++ components/Cards.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-auto-animate class="grid grid-cols-4 gap-4">
+    <div v-auto-animate class="grid grid-cols-4 grid-rows-2 gap-4">
         <div v-for="(card, index) in game.hand" :id="String(index)" draggable="true" class="card flex-wrap"
             :style="{ backgroundColor: card.color }" @dragstart="dragStart" @drop="insertCard" @dragover.prevent>
             {{ card.number }}</div>
